ecde2fc12d840d0c73a204415337ab7cee08d25d
allen91239 <s103062327@m103.nthu.edu.tw>
2016-06-02 18:42:28 +0800

    loading screen

--- translategame/WaitRoomFront.java
+++ translategame/WaitRoomFront.java
@@ -17,10 +17,15 @@ public class WaitRoomFront extends PApplet{
     TranslateGame parent;
     WaitRoomRear rear;
     PFont font;
-    
+    int pos = 100;
     //test variable
     PImage image;
-    
+    PImage mr0;
+    PImage mr1;
+    PImage mr2;
+    PImage mr3;
+    PImage mr4;
+    PImage mr5;
     WaitRoomFront(TranslateGame parent, WaitRoomRear rear){
         this.parent = parent;
         this.rear = rear;
@@ -34,13 +39,155 @@ public class WaitRoomFront extends PApplet{
         time = "";
         font = this.createFont("src/material/Tekton.otf", 64, true);
         textFont(font);
+        mr0 = loadImage("jump_r.png");
+        mr3 = loadImage("MushroomMoveR000.png");
+        mr4 = loadImage("MushroomMoveR001.png");
+        mr5 = loadImage("MushroomMoveR002.png");
     }
     
     @Override
     public void draw(){
         background(255);
         fill(0);
-        text(time, 200, 200);
+        int up = Integer.parseInt(time);
+        up = up % 10;
+        
+        switch(up){
+        case 0:
+        	pos = pos+5;
+        	image(mr3,pos,400);
+        	text("L        ", 100, 200+10);
+        	text("  o       ", 100, 200);
+        	text("    a      ", 100, 200);
+        	text("      d     ", 100, 200);
+        	text("        i    ", 100, 200);
+        	text("         n   ", 100, 200);
+        	text("           g  ", 100, 200);
+        	text("              : ", 100, 200);
+        	break;
+        case 1:
+        	pos = pos;
+        	image(mr3,pos,400);
+        	text("L        ", 100, 200);
+        	text("  o       ", 100, 200+10);
+        	text("    a      ", 100, 200);
+        	text("      d     ", 100, 200);
+        	text("        i    ", 100, 200);
+        	text("         n   ", 100, 200);
+        	text("           g  ", 100, 200);
+        	text("              : ", 100, 200);
+        	break;
+        case 2:
+        	pos = pos;
+        	image(mr4,pos,400);
+        	text("L        ", 100, 200);
+        	text("  o       ", 100, 200);
+        	text("    a      ", 100, 200+10);
+        	text("      d     ", 100, 200);
+        	text("        i    ", 100, 200);
+        	text("         n   ", 100, 200);
+        	text("           g  ", 100, 200);
+        	text("              : ", 100, 200);
+        	break;
+        case 3:
+        	pos = pos+5;
+        	image(mr4,pos,400);
+        	text("L        ", 100, 200);
+        	text("  o       ", 100, 200);
+        	text("    a      ", 100, 200);
+        	text("      d     ", 100, 200+10);
+        	text("        i    ", 100, 200);
+        	text("         n   ", 100, 200);
+        	text("           g  ", 100, 200);
+        	text("              : ", 100, 200);
+        	break;
+        case 4:
+        	pos = pos;
+        	image(mr5,pos,400);
+        	text("L        ", 100, 200);
+        	text("  o       ", 100, 200);
+        	text("    a      ", 100, 200);
+        	text("      d     ", 100, 200);
+        	text("        i    ", 100, 200+10);
+        	text("         n   ", 100, 200);
+        	text("           g  ", 100, 200);
+        	text("              : ", 100, 200);
+        	break;
+        case 5:
+        	pos = pos + 5;
+        	image(mr5,pos,400);
+        	text("L        ", 100, 200);
+        	text("  o       ", 100, 200);
+        	text("    a      ", 100, 200);
+        	text("      d     ", 100, 200);
+        	text("        i    ", 100, 200);
+        	text("         n   ", 100, 200+10);
+        	text("           g  ", 100, 200);
+        	text("              : ", 100, 200);
+        	break;
+        case 6:
+        	pos = pos;
+        	image(mr0,pos,400);
+        	text("L        ", 100, 200);
+        	text("  o       ", 100, 200);
+        	text("    a      ", 100, 200);
+        	text("      d     ", 100, 200);
+        	text("        i    ", 100, 200);
+        	text("         n   ", 100, 200);
+        	text("           g  ", 100, 200+10);
+        	text("              : ", 100, 200);
+        	break;
+        case 7:
+        	pos = pos;
+        	image(mr0,pos,400);
+        	text("L        ", 100, 200);
+        	text("  o       ", 100, 200);
+        	text("    a      ", 100, 200);
+        	text("      d     ", 100, 200);
+        	text("        i    ", 100, 200);
+        	text("         n   ", 100, 200);
+        	text("           g  ", 100, 200);
+        	text("              : ", 100, 200+10);
+        	break;
+        case 8:
+        	pos = pos;
+        	image(mr3,pos,400);
+        	text("L        ", 100, 200);
+        	text("  o       ", 100, 200);
+        	text("    a      ", 100, 200);
+        	text("      d     ", 100, 200);
+        	text("        i    ", 100, 200);
+        	text("         n   ", 100, 200);
+        	text("           g  ", 100, 200);
+        	text("              : ", 100, 200);
+        	break;
+        case 9:
+        	pos = pos;
+        	image(mr0,pos,400);
+        	text("L        ", 100, 200);
+        	text("  o       ", 100, 200);
+        	text("    a      ", 100, 200);
+        	text("      d     ", 100, 200);
+        	text("        i    ", 100, 200);
+        	text("         n   ", 100, 200);
+        	text("           g  ", 100, 200);
+        	text("              : ", 100, 200);
+        	break;
+        default:
+        	pos = pos;
+        	image(mr3,pos,400);
+        	text("L        ", 100, 200);
+        	text("  o       ", 100, 200);
+        	text("    a      ", 100, 200);
+        	text("      d     ", 100, 200);
+        	text("        i    ", 100, 200);
+        	text("         n   ", 100, 200);
+        	text("           g  ", 100, 200);
+        	text("              : ", 100, 200);
+        	break;
+        }
+        text(time+"%", 350, 200);
+        
     }
     
     public void setTime(String newTime){
